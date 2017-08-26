0afaac4db4995a514e66b1b987f1effde0b856e9
sosispanggang <sosispanggang.15@gmail.com>
2017-08-26 09:52:18 +0700

    ternyata habis formatting jadi error saudara-saudara

--- ajax_tes.php
+++ ajax_tes.php
@@ -2,45 +2,51 @@
 session_start();
 include "library/config.php";
 
-if (empty($_SESSION['id_tim']) or empty($_SESSION['password'])) {
-    header('location: login.php');
+if(empty($_SESSION['id_tim']) or empty($_SESSION['password']) ){
+   header('location: login.php');
 }
 
-// Proses ketika memilih jawaban
-
-if ($_GET['action'] == "kirim_jawaban") {
-    $rnilai          = mysqli_fetch_array(mysqli_query($mysqli, "SELECT * FROM nilai WHERE id_tes='$_POST[tes]' AND id_tim='$_SESSION[id_tim]'"));
-    $jawaban         = explode(",", $rnilai['jawaban']);
-    $index           = $_POST['index'];
+//Proses ketika memilih jawaban
+if($_GET['action']=="kirim_jawaban"){
+    $rnilai = mysqli_fetch_array(mysqli_query($mysqli, "SELECT * FROM nilai WHERE id_tes='$_POST[tes]' AND id_tim='$_SESSION[id_tim]'"));
+    $jawaban = explode(",", $rnilai['jawaban']);
+    $index = $_POST['index'];
     $jawaban[$index] = $_POST['jawab'];
-    $jawabanfix      = implode(",", $jawaban);
+
+    $jawabanfix = implode(",", $jawaban);
+    
     mysqli_query($mysqli, "UPDATE nilai SET jawaban='$jawabanfix', sisa_waktu='$_POST[sisa_waktu]' WHERE id_tes='$_POST[tes]' AND id_tim='$_SESSION[id_tim]'");
+
     echo "ok";
 }
 
-// Ketiak selesai tes
+//Ketiak selesai tes
+elseif($_GET['action']=="selesai_tes"){
+    $rnilai = mysqli_fetch_array(mysqli_query($mysqli, "SELECT * FROM nilai WHERE id_tes='$_POST[tes]' AND id_tim='$_SESSION[id_tim]'"));
 
-elseif ($_GET['action'] == "selesai_tes") {
-    $rnilai   = mysqli_fetch_array(mysqli_query($mysqli, "SELECT * FROM nilai WHERE id_tes='$_POST[tes]' AND id_tim='$_SESSION[id_tim]'"));
     $arr_soal = explode(",", $rnilai['acak_soal']);
-    $jawaban  = explode(",", $rnilai['jawaban']);
-    $jbenar   = 0;
-    for ($i = 0; $i < count($arr_soal); $i++) {
+    $jawaban = explode(",", $rnilai['jawaban']);
+    $jbenar = 0;
+    for($i=0; $i<count($arr_soal); $i++){
         $rsoal = mysqli_fetch_array(mysqli_query($mysqli, "SELECT * FROM soal WHERE id_tes='$_POST[tes]' AND id_soal='$arr_soal[$i]'"));
-        if ($rsoal['kunci'] == $jawaban[$i])
+        if($rsoal['kunci'] == $jawaban[$i])
             $jbenar++;
     }
-    
-    $nilai = $jbenar / count($arr_soal) * 100;
+
+    $nilai = $jbenar/count($arr_soal)*100;
+
     mysqli_query($mysqli, "UPDATE nilai SET jml_benar='$jbenar', nilai='$nilai' WHERE id_tes='$_POST[tes]' AND id_tim='$_SESSION[id_tim]'");
+
     mysqli_query($mysqli, "UPDATE peserta SET status='on' WHERE id_tim='$_SESSION[id_tim]'");
+
     echo "ok";
 }
-// Ketiak tamoil soal
-    elseif ($_GET['action'] == "tampil_soal") {
+
+//Ketiak tamoil soal
+elseif($_GET['action']=="tampil_soal"){
     mysqli_query($mysqli, "UPDATE nilai SET sisa_waktu='$_POST[sisa_waktu]' WHERE id_tes='$_POST[tes]' AND id_tim='$_SESSION[id_tim]'");
+
     echo "ok";
 }
 
 ?>
-
