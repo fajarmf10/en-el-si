8803a86671b700e2a9ad16efce3eb3037b5ed292
hendrywiranto <hendrywiranto24@gmail.com>
2017-09-16 14:27:45 +0700

    waktu serentak (dikurangi jam mulai)

--- home.php
+++ home.php
@@ -13,8 +13,35 @@ $tgl      = date('Y-m-d');
 $qtes     = mysqli_query($mysqli, "SELECT * FROM tes t1, edisites t2 WHERE t1.tanggal='$tgl' AND t1.id_tes=t2.id_tes AND t2.id_edisi='$_SESSION[edisi]' AND t2.aktif='Y'");
 $ttes     = mysqli_num_rows($qtes);
 $rtes     = mysqli_fetch_array($qtes);
+/*
 $qsession = mysqli_query($mysqli, "SELECT * FROM session WHERE id_tim='$_SESSION[id_tim]' AND id_tes='$rtes[id_tes]'");
 $hsession = mysqli_fetch_array($qsession);
+*/
+$qnilai = mysqli_query($mysqli, "SELECT * FROM nilai WHERE id_tes='$rtes[id_tes]' AND id_tim='$_SESSION[id_tim]'");
+$rnilai = mysqli_num_rows($qnilai);
+$tnilai = mysqli_fetch_array($qnilai);
+
+$jammulaidatetime = new DateTime($rtes['jam_mulai']);
+$logintime     = date("H:i:s");
+$logindatetime = new DateTime($logintime);
+
+if ($logindatetime > $jammulaidatetime and $rnilai > 0) {
+    $interval          = $logindatetime->diff($jammulaidatetime);
+    $elapsedtime       = $interval->format("%H:%i:%s");
+
+    //kurangi dengan durasi
+    $et = new DateTime($elapsedtime); 
+    $elapsedtimearr = explode(":", $elapsedtime);
+    $durasikurang   = dateseconds($elapsedtimearr[0], $elapsedtimearr[1], $elapsedtimearr[2]);
+        
+    $waktubaru = $rtes['waktu']*60 - $durasikurang;
+    if ($waktubaru < 0) {
+        $waktubaru = 1;
+    }
+    $hasilakhir = secondshour($waktubaru);
+    mysqli_query($mysqli, "UPDATE nilai SET sisa_waktu='$hasilakhir' WHERE id_tes='$rtes[id_tes]' AND id_tim='$_SESSION[id_tim]'");
+}
+/*
 if (mysqli_num_rows($qsession) < 1 and $rtes['id_tes'] != "0") {
     //tabel session, record waktu login
     //dapetin waktu sekarang, waktu login
@@ -64,7 +91,7 @@ if (mysqli_num_rows($qsession) < 1 and $rtes['id_tes'] != "0") {
         mysqli_query($mysqli, "UPDATE nilai SET sisa_waktu='$hasilakhir' WHERE id_tes='$rtes[id_tes]' AND id_tim='$_SESSION[id_tim]'");
     }
     
-}
+}*/
 
 //kalo ga ada tes yg aktif hari ini
 if ($ttes < 1) {
