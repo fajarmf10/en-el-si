b02305b97cca45261619c14c7b7ccf5f1f5bc796
hendrywiranto <hendrywiranto24@gmail.com>
2017-09-26 11:43:31 +0700

    ganti url

--- petunjuk.php
+++ petunjuk.php
@@ -15,7 +15,7 @@ if (empty($_SESSION['id_tim']) or empty($_SESSION['password'])) {
 	<li>Kompetisi dimulai pada 9.00 WIB dan berakhir pada 10.40 WIB (100 menit)</li>
 	<li>Tidak ada penambahan waktu bagi yang terlambat mengerjakan</li>
 	<li>Penilaian : Benar +3, Salah -1, Tidak menjawab 0</li>
-	<li>Pastikan minimal salah satu anggota tim anda telah tergabung dalam grup Facebook NLC Online (URL)</li>
+<li>Pastikan minimal salah satu anggota tim anda telah tergabung dalam grup <a href="https://www.facebook.com/groups/129701847678188/">Facebook NLC Online</a></li>
 	<li>Grup facebook ini digunakan untuk alat komunikasi antara peserta dan panitia</li>
 	<li>Tidak ada revisi pada soal</li>
 	<li>Apabila ada kesalahan teknis dari peserta, maka bukan tanggung jawab panitia</li>
